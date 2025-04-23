d8327ae61df6b067d4ad41dcf7ba08fabe707415
Laggy_Mag <69040612+Ha2k6@users.noreply.github.com>
2025-04-23 18:28:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ app = FastAPI()
 
 # Enable CORS
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Change this to specific domains for security
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["*"],  # Change this to specific domains for security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Load the pre-trained model
-classifier = pipeline("text-classification", model="mrm8488/bert-tiny-finetuned-fake-news-detection")
-
+classifier = pipeline("text-classification", model="lvwerra/distilbert-imdb")
 
 # API Route for Fake News Detection
 @app.get("/predict/")
 def detect_fake_news(text: str):
-    result = classifier(text)
-    label = result[0]['label']
-    return {"text": text, "prediction": "Fake News" if label == 'NEGATIVE' else "Real News"}
+    result = classifier(text)
+    label = result[0]['label']
+    return {"text": text, "prediction": "Fake News" if label == 'NEGATIVE' else "Real News"}
+
+
